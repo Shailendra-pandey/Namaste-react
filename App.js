2ff9c5064fb22ea27d703ea1e0de6c017b041d81
shailendra-pandey <shailendrapandey028@gmail.com>
2024-05-07 18:34:35 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // const heading = React.createElement(
 //   "h1",
@@ -7,15 +7,30 @@ import ReactDOM from "react-dom";
 //   "Hello World from React!"
 // );
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 tag"),
-    React.createElement("h2", {}, "I am a H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a H1 tag"),
-    React.createElement("h2", {}, "I am a H2 tag"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am a H1 tag"),
+//     React.createElement("h2", {}, "I am a H2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am a H1 tag"),
+//     React.createElement("h2", {}, "I am a H2 tag"),
+//   ]),
+// ]);
+
+// const jsxHeading = <h1 id="heading">Namaste React using JSX🚀</h1>;
+
+const Title = () => <h1 id="heading">Namaste React using JSX🚀</h1>;
+const name = "Shailendra";
+
+// Component Composition
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    <h1>Heading Component by {name}</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
