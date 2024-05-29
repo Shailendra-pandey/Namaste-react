3b38b0b3db354932acb5cdc0ff54b1200ae13b57
shailendra-pandey <shailendrapandey028@gmail.com>
2024-05-29 16:08:21 +0530

    Episode-8

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import UserClass from "./UserClass";
+import { useState } from "react";
+
 const About = () => {
+  const [name] = useState("Shailendra");
+  const [location] = useState("Noida");
+  const [contact] = useState("shailendra123");
   return (
     <div>
       <h1>About</h1>
       <h2>This is about page</h2>
+      <UserClass name={name} location={location} contact={contact} />
     </div>
   );
 };
